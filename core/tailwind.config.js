04dfa48900a6c497175a89d39a971dcb06ff7413
nothing <alexandermatthew25@gmail.com>
2025-01-27 12:50:33 +0000

    migrate to v4 because of tailwind

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html"
-  ],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+};
